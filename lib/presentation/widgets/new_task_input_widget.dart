bd657deab6defd80219f123b778326855d82677c
Thomas Viana <thomasviana16@gmail.com>
2021-12-18 14:22:19 -0500

    Update new task input widget

--- lib/presentation/widgets/new_task_input_widget.dart
+++ lib/presentation/widgets/new_task_input_widget.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class NewTaskInput extends StatefulWidget {
-  const NewTaskInput({Key? key}) : super(key: key);
+  const NewTaskInput({
+    Key? key,
+  }) : super(key: key);
 
   @override
   _NewTaskInputState createState() => _NewTaskInputState();
@@ -12,39 +14,47 @@ class NewTaskInput extends StatefulWidget {
 
 class _NewTaskInputState extends State<NewTaskInput> {
   DateTime? newTaskDate;
+  Tag? selectedTag;
   late TextEditingController controller;
 
   @override
   void initState() {
-    controller = TextEditingController();
     super.initState();
+    controller = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8.0),
       child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
           _buildTextField(context),
+          _buildTagSelector(context),
           _buildDateButton(context),
         ],
       ),
     );
   }
 
-  Widget _buildTextField(BuildContext context) {
+  Expanded _buildTextField(BuildContext context) {
     return Expanded(
+      flex: 1,
       child: TextField(
         controller: controller,
-        decoration: const InputDecoration(hintText: 'Task name'),
+        decoration: const InputDecoration(hintText: 'Task Name'),
         onSubmitted: (inputName) {
           final dao = Provider.of<TaskDao>(context, listen: false);
           final task = TasksCompanion(
             name: Value(inputName),
             dueDate: Value(newTaskDate),
+            tagName: selectedTag != null
+                ? Value(selectedTag!.name)
+                : const Value(null),
           );
+          print(task.name.value);
+          print(task.tagName.value);
           dao.insertTask(task);
           resetValuesAfterSubmit();
         },
@@ -52,23 +62,81 @@ class _NewTaskInputState extends State<NewTaskInput> {
     );
   }
 
+  StreamBuilder<List<Tag>> _buildTagSelector(BuildContext context) {
+    final dao = Provider.of<TagDao>(context, listen: false);
+    return StreamBuilder<List<Tag>>(
+      stream: dao.watchTags(),
+      builder: (context, snapshot) {
+        final tags = snapshot.data ?? [];
+        print(tags);
+        DropdownMenuItem<Tag> dropdownFromTag(Tag tag) {
+          return DropdownMenuItem(
+            value: tag,
+            child: GestureDetector(
+              onLongPress: () => dao.deleteTag(tag),
+              child: Row(
+                children: <Widget>[
+                  Text(tag.name),
+                  const SizedBox(width: 5),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(tag.color),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+
+        final dropdownMenuItems =
+            tags.map((tag) => dropdownFromTag(tag)).toList()
+              // Add a "no tag" item as the first element of the list
+              ..insert(
+                0,
+                const DropdownMenuItem(
+                  value: null,
+                  child: Text('No Tag'),
+                ),
+              );
+
+        return Expanded(
+          child: DropdownButton(
+            onChanged: (Tag? tag) {
+              setState(() {
+                selectedTag = tag;
+              });
+            },
+            isExpanded: true,
+            value: selectedTag,
+            items: dropdownMenuItems,
+          ),
+        );
+      },
+    );
+  }
+
   IconButton _buildDateButton(BuildContext context) {
     return IconButton(
-        onPressed: () async {
-          newTaskDate = await showDatePicker(
-              context: context,
-              initialDate: DateTime.now(),
-              firstDate: DateTime(2010),
-              lastDate: DateTime(2050));
-        },
-        icon: const Icon(
-          Icons.calendar_today,
-        ));
+      icon: const Icon(Icons.calendar_today),
+      onPressed: () async {
+        newTaskDate = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(2010),
+          lastDate: DateTime(2050),
+        );
+      },
+    );
   }
 
   void resetValuesAfterSubmit() {
     setState(() {
       newTaskDate = null;
+      selectedTag = null;
       controller.clear();
     });
   }
